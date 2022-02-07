4573d3c2dc41c6cb7fef54136a29421c21dfe6b8
Jose Barroso <barroso.j52@gmail.com>
2022-02-06 19:13:34 -0800

    Overall app layout was created. New fragments were created for most user stories, if not all. Navigation bar has also been started for easy access to new screens.

---
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/MovieScrollingFragment.java
@@ -0,0 +1,21 @@
+package com.daclink.drew.sp22.cst438_project01_starter;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class MovieScrollingFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movie_scrolling, container, false);
+    }
+}
---
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/SettingsFragment2.java
@@ -0,0 +1,13 @@
+package com.daclink.drew.sp22.cst438_project01_starter;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment2 extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
