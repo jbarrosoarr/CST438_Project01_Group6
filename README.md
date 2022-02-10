d1e3e98a988bd4c4b5239c2d01612098f8fd6513
Jose Barroso <barroso.j52@gmail.com>
2022-02-10 12:43:35 -0800

    User profile was created. Buttons were added for viewing user's list, viewing search history, changing password and logging out. Navigation bar was all started.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Open Movie API project
+
+### Team Members 
+
+CST438 group 6:
+- [Jose Barroso A](https://github.com/jbarrosoarr)
+- [Pedro Gutierrez Jr.](https://github.com/PedroG1018)
+- [Isai Molina](https://github.com/isaiM6)
+- [Sterling 'Hal' Halberstadt](https://github.com/halHalberstadt)
+
+### Project Description
+
+&nbsp;&nbsp;&nbsp;Our application uses the [open movie API](http://www.omdbapi.com/) in order to present the user with a user-friendly way to get reccomendations for movies. 
+The application takes in basic title and year information from the user then displays that movie to the user.
+
+### API link:
+
+&nbsp;&nbsp;&nbsp;
+http://www.omdbapi.com/
+
+### API description:<br>
+&nbsp;&nbsp;&nbsp;
+Open Movie API uses some basic information about the movie then returns aspects of the movie like genere, a plot summary, ratings, awards, release date, movie poster, etc.
+
+### Links
+
+5. Links to any resources you have found useful
+   - Tutorials on Recycler views
+   - Guide to using Room
+   - Android development for the forgetful
---
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/UserProfile.java
@@ -0,0 +1,39 @@
+package com.daclink.drew.sp22.cst438_project01_starter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.firestore.auth.User;
+
+public class UserProfile extends AppCompatActivity {
+    Button logoutBtn;
+    Button changePW;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        logoutBtn = (Button)findViewById(R.id.logoutBtn);
+        changePW = (Button)findViewById(R.id.changePW);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(UserProfile.this, MainActivity.class);
+                startActivity(new Intent(UserProfile.this, MainActivity.class));
+            }
+        });
+
+        changePW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserProfile.this, ChangePasswordActivity.class));
+            }
+        });
+    }
+}
