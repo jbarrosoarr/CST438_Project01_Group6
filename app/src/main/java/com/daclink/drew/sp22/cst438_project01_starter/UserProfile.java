462fba16cef6fabbc5e635cfb30a60aed6a79c34
Jose Barroso <barroso.j52@gmail.com>
2022-02-12 00:41:45 -0800

    Change password button now works, linked to new activity.

--- app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/UserProfile.java
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/UserProfile.java
@@ -11,4 +11,26 @@ import com.google.firebase.firestore.auth.User;
 
 public class UserProfile extends AppCompatActivity {
 
+    private Button changePwBtn;
+    private Button logoutBtn;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        changePwBtn = (Button) findViewById(R.id.changePW);
+        changePwBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openChangePasswordActivity();
+            }
+        });
+
+    }
+
+        public void openChangePasswordActivity() {
+            Intent intent = new Intent(this, ChangePasswordActivity.class);
+            startActivity(intent);
+    }
+
 }
